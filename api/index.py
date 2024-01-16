32ff1cd04c714e2e7f518b1ed105237fc70e783f
iyur <iyurrr@gmail.com>
2024-01-17 00:08:45 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,1 @@
-from http.server import BaseHTTPRequestHandler
-
-class handler(BaseHTTPRequestHandler):
-
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        #request = requests('https://rest.fnar.net/global/countries')
-        self.wfile.write('test'.encode('utf-8'))
-        return
+return "just a test"
