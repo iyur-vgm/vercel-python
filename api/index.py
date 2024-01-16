d7e2665563ff531f850c85fd629d8d0dca165fdb
iyur <iyurrr@gmail.com>
2024-01-16 23:49:52 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -7,5 +7,5 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('Content-type','text/plain')
         self.end_headers()
         request = requests('https://rest.fnar.net/global/countries')
-        self.wfile.write('Hello, world!'.encode('utf-8'))
+        self.wfile.write(request.encode('utf-8'))
         return
